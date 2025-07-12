67ca91d2392f7f641b77a349ffab1270b95c1f50
Tanmay Gaikwad <gaikwadtanmay723@gmail.com>
2025-07-12 23:59:04 +0530

    chore:Add input field & configure dynamic container widget

--- front_end/app.py
+++ front_end/app.py
@@ -1,17 +1,53 @@
 import streamlit as st
-from back_end.helpers import fetch_reddit_comments
+st.set_page_config(layout="wide")
+import matplotlib.pyplot as plt
+from PIL import Image
+from api import generate_output
 
-st.title("Platform Analyzer")
 
-link = st.text_input("Enter the URL of the reddit post : ")
 
-def check_url(url:str) -> bool:
-    if url.startswith("https://www.reddit.com/r/") or url.startswith("https://reddit.com/r/"):
-      print(fetch_reddit_comments(url))
-      return True
-    else:
-      return False
+st.session_state["container2"] = False
 
-comments = st.button("Analyze", on_click=check_url, args=(link,))
+# st.title("Platform Analyzer")
+st.markdown(
+    """
+    <style>
+    .custom-title {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        padding: 10px;
+        z-index: 99999999;
+        text-align: center;
+        font-family: Inter, monospace;
+        font-size: 60px;
+        font-weight: 700;
+        line-height: 120%;
+        padding-top: 24px;
+    }
+    </style>
+    <span class='custom-title'>Platform Analyzer</span>
+    """,
+    unsafe_allow_html=True
+)
 
-print(comments)
+with st.container(height=200, border=False):
+    st.subheader("Enter the link of url")
+    url = st.text_input("", width=1000, label_visibility="collapsed", placeholder="https://reddit.com/")
+    if st.button("Analyze"):
+        negatives_sorted, neutral_sorted, positive_sorted = generate_output(url)
+        st.session_state["container2"] = True
+
+if st.session_state["container2"]:
+  with st.container():
+    col1, col2, col3 = st.columns((1,1,1), gap="medium", vertical_alignment="center", border=True)
+    col1.header("Negative Cloud")
+    col1.image(Image.open("negative_cloud.png"), use_container_width=True)
+    col1.dataframe(negatives_sorted[0], use_container_width=True, hide_index=True, row_height=30)
+    col2.header("Neutral Cloud")
+    col2.image(Image.open("neutral_cloud.png"), use_container_width=True)
+    col2.dataframe(neutral_sorted[0], use_container_width=True, hide_index=True, row_height=30)
+    col3.header("Positive Cloud")
+    col3.image(Image.open("positive_cloud.png"), use_container_width=True)
+    col3.dataframe(positive_sorted[0], use_container_width=True, hide_index=True, row_height=30)
